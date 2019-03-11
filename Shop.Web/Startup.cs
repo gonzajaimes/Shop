c5a10d56425bb87839b4c4c8b332f132e2d91128
gonzajaimes <gonzaiames@hotmail.com>
2019-03-10 19:49:06 -0500

    change config parameters

--- Shop.Web/Startup.cs
+++ Shop.Web/Startup.cs
@@ -53,6 +53,8 @@ namespace Shop.Web
 
             services.AddIdentity<User, IdentityRole>(cfg =>
             {
+                cfg.Tokens.AuthenticatorTokenProvider = TokenOptions.DefaultAuthenticatorProvider;
+                cfg.SignIn.RequireConfirmedEmail = true;
                 cfg.User.RequireUniqueEmail = true;
                 cfg.Password.RequireDigit = false;
                 cfg.Password.RequiredUniqueChars = 0;
