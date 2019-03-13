395b35755f3ab81d811293df71006484b508244d
gjaimes <gjaimes@emeingenieria.com.co>
2019-03-13 17:43:57 -0500

    User  name maxlength

--- Shop.Web/Data/Entities/User.cs
+++ Shop.Web/Data/Entities/User.cs
@@ -5,8 +5,10 @@
 
     public class User : IdentityUser
     {
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
         public string FirstName { get; set; }
 
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
         public string LastName { get; set; }
 
         [Display(Name = "Full Name")]
